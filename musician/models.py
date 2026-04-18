3b7d82ac9dc0786dae9a90ed4e1b1cbfb7a9b0aa
Mateusz <mateusz.ruszczynski@gmail.com>
2026-04-18 22:10:03 +0200

    feat: flake8 issue

--- musician/models.py
+++ musician/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 class Musician(models.Model):
     first_name = models.CharField(max_length=63)
     last_name = models.CharField(max_length=63)
